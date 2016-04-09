b7176856b40d94a4a8629bed31ebe99498c35f42
gimapei <gimapei@gmail.com>
2016-04-09 17:04:36 +0900

    end of lecture

--- R-script/chap08_Informal.R
+++ R-script/chap08_Informal.R
@@ -263,3 +263,248 @@ lab <- paste(names(topWord), "\n", pct, "%")
 # (5) 파이차트에 단어와 백분율을 레이블로 적용 
 pie(topWord, main="SNS 빅데이터 관련 토픽분석", col=rainbow(10), cex=0.8, labels=lab)
 
+
+
+
+######### 3번째 시간
+###################################################
+# 단계2 - 연관어 분석(단어와 단어 사이 연관성 분석) 
+#   - 시각화 : 연관어 네트워크 시각화, 근접중심성 시각화
+###################################################
+
+# 한글 처리를 위한 패키지 설치
+install.packages("rJava")
+Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_77')
+library(rJava) # 아래와 같은 Error 발생 시 Sys.setenv()함수로 java 경로 지정
+
+install.packages("KoNLP")
+library(KoNLP) # rJava 라이브러리가 필요함
+
+
+# 1.텍스트 파일 가져오기
+#----------------------------------------------------
+marketing <- file("C:/Rwork/Part-II/marketing.txt", encoding="UTF-8")
+marketing2 <- readLines(marketing) # 줄 단위 데이터 생성
+# incomplete final line found on - Error 발생 시 UTF-8 인코딩 방식으로 재 저장
+close(marketing) 
+head(marketing2) # 앞부분 6줄 보기 - 줄 단위 문장 확인 
+#----------------------------------------------------
+
+# 2. 줄 단위 단어 추출
+#----------------------------------------------------
+# Map()함수 이용 줄 단위 단어 추출 : Map(f, ...) -> base)
+lword <- Map(extractNoun, marketing2) 
+lword
+
+length(lword) # [1] 472
+lword <- unique(lword) # 중복제거1(전체 대상)
+length(lword) # [1] 353(19개 제거)
+lword <- sapply(lword, unique) # 중복제거2(줄 단위 대상) 
+length(lword) # [1] 352(1개 제거)
+str(lword) # List of 353
+lword # 추출 단어 확인
+#----------------------------------------------------
+
+# 3. 전처리
+#----------------------------------------------------
+# 1) 길이가 2~4 사이의 단어 필터링 함수 정의
+filter1 <- function(x){
+  nchar(x) <= 4 && nchar(x) >= 2 && is.hangul(x)
+}
+# 2) Filter(f,x) -> filter1() 함수를 적용하여 x 벡터 단위 필터링 
+filter2 <- function(x){
+  Filter(filter1, x)
+}
+# is.hangul() : KoNLP 패키지 제공
+# Filter(f, x) : base
+# nchar() : base -> 문자 수 반환
+
+# 3) 줄 단어 대상 필터링
+lword <- sapply(lword, filter2)
+
+lword # 추출 단어 확인(길이 1개 단어 삭제됨)
+
+
+# 4. 트랜잭션 생성 : 연관분석을 위해서 단어를 트랜잭션으로 변환
+#----------------------------------------------------
+# arules 패키지 설치
+install.packages("arules")
+library(arules) 
+#----------------------------
+# arules 패키지 제공 기능
+# - Adult,Groceries 데이터 셋
+# - as(),apriori(),inspect(),labels(),crossTable()
+#-----------------------------
+wordtran <- as(lword, "transactions") # lword에 중복데이터가 있으면 error발생
+wordtran
+
+wordtable <- crossTable(wordtran) # 교차표 작성
+wordtable
+
+
+# 5.단어 간 연관규칙 산출
+#----------------------------------------------------
+# 트랜잭션 데이터를 대상으로 지지도와 신뢰도를 적용하여 연관규칙 생성
+# 형식) apriori(data, parameter = NULL, appearance = NULL, control = NULL)
+# data :  object of class transactions, 
+# parameter : support 0.1, conﬁdence 0.8, and maxlen 10(연관단어 최대수) 
+tranrules <- apriori(wordtran, parameter=list(supp=0.25, conf=0.05)) 
+# 0.22 -> 84 rules, supp=0.25 -> 59 rules
+# # 지지도와 신뢰도를 높이면 발견되는 규칙의 수가 줄어든다.
+
+inspect(tranrules) # 연관규칙 생성 결과(59개) 보기
+
+
+# 6.연관어 시각화 - rulemat[c(34:63),] # 연관규칙 결과- {} 제거(1~33)
+#----------------------------------------------------
+# (1) 연관단어 시각화를 위해서 데이터 구조 변경
+rules <- labels(tranrules, ruleSep=" ") 
+class(rules)
+#[1] "character"
+rules <- sapply(rules, strsplit, " ",  USE.NAMES=F) 
+rulemat <- do.call("rbind", rules)
+# sapply(), do.call() # base 패키지
+rulemat
+class(rulemat)
+#[1] "matrix"
+
+
+# (2) 연관어 시각화를 위한 igraph 패키지 설치
+install.packages("igraph") # graph.edgelist(), plot.igraph(), closeness() 함수 제공
+library(igraph)   
+
+# (3) edgelist보기 - 연관단어를 정점 형태의 목록 제공 
+
+ruleg <- graph.edgelist(rulemat[c(12:59),], directed=F) # [1,]~[11,] "{}" 제외
+ruleg
+
+# (4) edgelist 시각화
+plot.igraph(ruleg, vertex.label=V(ruleg)$name,
+            vertex.label.cex=1.2, vertex.label.color='black', 
+            vertex.size=20, vertex.color='green', vertex.frame.color='blue')
+# 정점(타원) 레이블 속성
+# vertex.label=레이블명,vertex.label.cex=레이블 크기, vertex.label.color=레이블색
+# 정점(타원) 속성 
+# vertext.size= 정점 크기, vertex.color=정점 색, vertex.frame.color=정점 테두리 색
+
+# 7.단어 근접중심성(closeness centrality) 파악
+#----------------------------------------------------
+closen <- closeness(ruleg) # edgelist 대상 단어 근접중심성 생성 
+#closen <- closen[c(2:8)] # {} 항목제거
+closen <- closen[c(1:10)] # 상위 1~10 단어 근접중심성 보기
+plot(closen, col="red",xaxt="n", lty="solid", type="b", xlab="단어", ylab="closeness") 
+points(closen, pch=16, col="navy") 
+axis(1, seq(1, length(closen)), V(ruleg)$name[c(1:10)], cex=5)
+# 중심성 : 노드(node)의 상대적 중요성을 나타내는 척도이다.
+# plot, points(), axis() : graphics 패키지(기존 설치됨)
+
+#---------------------------------------------------------------
+#<연관어분석 관련 연습문제>
+#---------------------------------------------------------------
+
+
+### 4번째 시간
+#############################################
+# 단계3 - 감성 분석(단어의 긍정/부정 분석) 
+#  - 시각화 : 파랑/빨강 -> 불만고객 시각화
+#############################################
+
+# 1) 데이터 가져오기("c:/Rwork/Part-II/reviews.csv") 
+data<-read.csv(file.choose()) # file.choose() 파일 선택
+head(data,2)
+dim(data) # 100   2
+str(data) # 변수명 : company, review <- 고객 인터뷰 내용
+
+# 2) 단어 사전에 단어추가
+
+# 긍정단어와 부정단어를 카운터하여 긍정/부정 형태로 빈도 분석
+#neg.txt : 부정어 사전
+#pos.txt : 긍정어 사전
+
+# (1) 긍정어/부정어 영어 사전 가져오기
+setwd("C:/Rwork/Part-II")
+posDic <- readLines("posDic.txt")
+negDic <- readLines("negDic.txt")
+length(posDic) # 2006
+length(negDic) # 4783
+
+
+# (2) 긍정어/부정어 단어 추가 
+posDic.final <-c(posDic, 'victor')
+negDic.final <-c(negDic, 'vanquished')
+# 마지막에 단어 추가
+posDic.final; 
+negDic.final 
+
+
+# 3) 감성 분석 함수 정의-sentimental
+
+# (1) 문자열 처리를 위한 패키지 로딩 
+library(plyr) # laply()함수 제공
+library(stringr) # str_split()함수 제공
+
+# (2) 감성분석을 위한 함수 정의
+sentimental = function(sentences, posDic, negDic){
+  
+  scores = laply(sentences, function(sentence, posDic, negDic) {
+    
+    sentence = gsub('[[:punct:]]', '', sentence) #문장부호 제거
+    sentence = gsub('[[:cntrl:]]', '', sentence) #특수문자 제거
+    sentence = gsub('\\d+', '', sentence) # 숫자 제거
+    sentence = tolower(sentence) # 모두 소문자로 변경(단어가 모두 소문자 임)
+    
+    word.list = str_split(sentence, '\\s+') # 공백 기준으로 단어 생성 -> \\s+ : 공백 정규식, +(1개 이상) 
+    words = unlist(word.list) # unlist() : list를 vector 객체로 구조변경
+    
+    pos.matches = match(words, posDic) # words의 단어를 posDic에서 matching
+    neg.matches = match(words, negDic)
+    
+    pos.matches = !is.na(pos.matches) # NA 제거, 위치(숫자)만 추출
+    neg.matches = !is.na(neg.matches)
+    
+    score = sum(pos.matches) - sum(neg.matches) # 긍정 - 부정    
+    return(score)
+  }, posDic, negDic)
+  
+  scores.df = data.frame(score=scores, text=sentences)
+  return(scores.df)
+}
+
+# 4) 감성 분석 : 두번째 변수(review) 전체 레코드 대상 감성분석
+result<-sentimental(data[,2], posDic.final, negDic.final)
+result
+names(result) # "score" "text" 
+dim(result) # 100   2
+result$text
+result$score # 100 줄 단위로 긍정어/부정어 사전을 적용한 점수 합계
+
+# score값을 대상으로 color 칼럼 추가
+result$color[result$score >=1] <- "blue"
+result$color[result$score ==0] <- "green"
+result$color[result$score < 0] <- "red"
+
+# 감성분석 결과 차트보기
+plot(result$score, col=result$color) # 산포도 색생 적용
+barplot(result$score, col=result$color, main ="감성분석 결과화면") # 막대차트
+
+
+# 5) 단어의 긍정/부정 분석 
+
+# (1) 감성분석 빈도수 
+table(result$color)
+
+# (2) score 칼럼 리코딩 
+result$remark[result$score >=1] <- "긍정"
+result$remark[result$score ==0] <- "중립"
+result$remark[result$score < 0] <- "부정"
+
+sentiment_result<- table(result$remark)
+sentiment_result
+
+# (3) 제목, 색상, 원크기
+pie(sentiment_result, main="감성분석 결과", 
+    col=c("blue","red","green"), radius=0.8) # ->  1.2
+
+
+
+
